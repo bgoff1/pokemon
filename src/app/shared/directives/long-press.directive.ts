419c285f03a3e4935d7a514bebb3c5d3081c3618
Judah Goff <judahgoff@gmail.com>
2020-04-07 23:58:10 -0500

    Fix bug with long press directive

--- src/app/shared/directives/long-press.directive.ts
+++ src/app/shared/directives/long-press.directive.ts
@@ -1,47 +1,26 @@
-import {
-  Directive,
-  EventEmitter,
-  HostListener,
-  Input,
-  Output,
-  OnDestroy
-} from '@angular/core';
+import { Directive, EventEmitter, HostListener, Output } from '@angular/core';
 
 @Directive({
   selector: '[longPress]'
 })
-export class LongPressDirective implements OnDestroy {
-  @Input() public longPress = 500;
-  @Output() public release: EventEmitter<any> = new EventEmitter();
+export class LongPressDirective {
+  timeout: any;
 
-  start: number;
-  timeoutRef: any;
+  @Output()
+  release = new EventEmitter();
 
-  public ngOnDestroy(): void {
-    this.clearTimer();
+  @HostListener('touchstart', ['$event'])
+  @HostListener('mousedown', ['$event'])
+  startPress() {
+    this.timeout = setTimeout(() => {
+      this.release.emit();
+    }, 500);
   }
 
-  clearTimer() {
-    if (Date.now() - this.start < this.longPress) {
-      clearTimeout(this.timeoutRef);
-    }
-  }
-
-  @HostListener('pointerup')
-  public onMouseUp(): void {
-    this.clearTimer();
-  }
-
+  @HostListener('touchend')
+  @HostListener('mouseup')
   @HostListener('mouseleave')
-  public onMouseLeave(): void {
-    this.clearTimer();
-  }
-
-  @HostListener('pointerdown')
-  public onMouseDown(): void {
-    this.start = Date.now();
-    this.timeoutRef = setTimeout(() => {
-      this.release.next();
-    }, this.longPress);
+  endPress() {
+    clearTimeout(this.timeout);
   }
 }
